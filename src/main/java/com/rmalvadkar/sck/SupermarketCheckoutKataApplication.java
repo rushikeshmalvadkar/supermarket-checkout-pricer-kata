7ae1fd17d285f9027bdcd725cfb53f5ab93a0f4a
Rushikesh Malvadkar <rushi@dev.com>
2025-06-27 12:16:05 +0530

    Initial commit

---
+++ src/main/java/com/rmalvadkar/sck/SupermarketCheckoutKataApplication.java
@@ -0,0 +1,13 @@
+package com.rmalvadkar.sck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketCheckoutKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketCheckoutKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmalvadkar/sck/SupermarketCheckoutKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmalvadkar.sck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketCheckoutKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
